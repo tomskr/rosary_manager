1632eec6635b8786d6ad2d321119cd6a9d071aff
TomaszSkrzypek <tomskr3.ts@gmail.com>
2019-11-22 11:16:59 +0100

    Add Css and js callendar elements

--- src/main/java/pl/tomskr/rosary_manager/controller/HarmonogramController.java
+++ src/main/java/pl/tomskr/rosary_manager/controller/HarmonogramController.java
@@ -1,12 +1,21 @@
 package pl.tomskr.rosary_manager.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.tomskr.rosary_manager.service.RosaryGroupService;
 
 @Controller
 public class HarmonogramController {
+    private final RosaryGroupService rosaryGroupService;
+
+    public HarmonogramController(RosaryGroupService rosaryGroupService) {
+        this.rosaryGroupService = rosaryGroupService;
+    }
+
     @RequestMapping(value = "index/harmonogram")
-    public String openHarmonogram(){
+    public String openHarmonogram(Model model){
+        model.addAttribute("rosaryGroups", rosaryGroupService.getRosaryGroups());
         return "harmonogram";
     }
 }
--- src/main/resources/static/js/harmonogram.js
+++ src/main/resources/static/js/harmonogram.js
@@ -3,6 +3,14 @@
 
 $(document).ready(function(){
 
-    // jQuery methods go here...
-
+    var mieSkr = ['STY', 'LUT', 'MAR', 'KWI', 'MAJ', 'CZE', 'LIP', 'SIE', 'WRZ', 'PAZ' , 'LIS', 'GRU'];
+    for(var i = 0; i< mieSkr.length;i++){
+        var dt = new Date(2020, i,01);
+        while(dt.getDay()!=0){
+            dt.setDate(dt.getDate() + 1)
+        }
+        $("#" + mieSkr[i]).text(dt.getDate() + " . " + mieSkr[i]);
+    }
 });
+
+
