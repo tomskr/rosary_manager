7c0173df551343ccf87ee79b05b1b388ae70e872
TomaszSkrzypek <tomskr3.ts@gmail.com>
2021-11-30 13:26:56 +0100

    Added Data Initializer

---
+++ src/main/java/pl/tomskr/rosary_manager/bootstrap/DataInitializer.java
@@ -0,0 +1,31 @@
+package pl.tomskr.rosary_manager.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import pl.tomskr.rosary_manager.domain.RosaryGroup;
+import pl.tomskr.rosary_manager.repository.RosaryGroupRepository;
+import pl.tomskr.rosary_manager.service.RosaryGroupService;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    private final RosaryGroupService rosaryGroupService;
+
+    public DataInitializer(RosaryGroupService rosaryGroupService) {
+        this.rosaryGroupService = rosaryGroupService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        RosaryGroup group000 = new RosaryGroup(1, "Matki Bożej Fatimskiej");
+        rosaryGroupService.save(group000);
+
+        RosaryGroup group001 = new RosaryGroup(2, "Św. Teresy od Dziec. Jezus");
+        rosaryGroupService.save(group000);
+
+        RosaryGroup group002 = new RosaryGroup(3, "Matki Bożej Fatimskiej");
+        rosaryGroupService.save(group000);
+
+
+    }
+}
--- src/main/java/pl/tomskr/rosary_manager/domain/RosaryGroup.java
+++ src/main/java/pl/tomskr/rosary_manager/domain/RosaryGroup.java
@@ -1,28 +1,56 @@
 package pl.tomskr.rosary_manager.domain;
 
-import lombok.Data;
-import org.hibernate.annotations.DynamicUpdate;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.core.annotation.Order;
-import org.springframework.transaction.annotation.Transactional;
-
 import javax.persistence.*;
-import java.util.*;
 
-@Data
 @Entity
 public class RosaryGroup{
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private int groupNumber;
 
-    @OneToMany(orphanRemoval = true, cascade = CascadeType.ALL)
-    @JoinColumn(name = "group_id")
-    private List<Event> eventList = new ArrayList<>();
-    @OneToMany
-    private List<RosaryMember> rosaryMembers  = new ArrayList<>();
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Integer groupNumber;
+    private String name;
+
+
+//    @JoinColumn(name = "group_id")
+//    @OneToMany(orphanRemoval = true, cascade = CascadeType.ALL)
+//    private List<Event> eventList = new ArrayList<>();
+//
+//    @OneToMany
+//    private List<RosaryMember> rosaryMembers  = new ArrayList<>();
+
+
+    public RosaryGroup() {
+    }
+
+    public RosaryGroup(Integer groupNumber, String name) {
+        this.groupNumber = groupNumber;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getGroupNumber() {
+        return groupNumber;
+    }
+
+    public void setGroupNumber(Integer groupNumber) {
+        this.groupNumber = groupNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 
 }
